c435e4a9e5d4b9d8f14a9a75ba2e3cd255162d98
Harshala Gaikwad <harshala_gaikwad@persistent.com>
2022-08-24 10:40:52 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { nanoid } from "nanoid";
+import NotesList from "./components/NotesList";
+import Search from './components/Search';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [notes, setNotes] = useState([{
+    id: nanoid(),
+    text: "This is my first note!",
+    date:"15/10/2021",
+  },
+  {
+    id: nanoid(),
+    text: "This is my second note!",
+    date:"15/10/2021",
+  },
+]);
+
+const [searchText, setSearchText] = useState('');
+const addNote = (text) =>{
+  const date = new Date();
+  const newNote = {
+    id: nanoid(),
+    text: text,
+    date: date.toLocaleDateString()
+  }
+  const newNotes = [...notes, newNote];
+  setNotes(newNotes);
 }
-
+const deleteNote = (id) => {
+  const newNotes = notes.filter((note)=> note.id !== id);
+  setNotes(newNotes);
+}
+  return (
+    <div className="container">
+      <Search />
+      <NotesList notes={notes} handleAddNote={addNote}
+      handleDeleteNote = {deleteNote}/>
+    </div>
+  )
+};
 export default App;
